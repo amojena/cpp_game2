c2b8f00de52fa1b43a652d9c2cc2a0264a9f6782
amojena <amojenar@purdue.edu>
2017-11-24 19:50:14 -0500

    Created a player class

---
+++ Player.hpp
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include "Item.hpp"
+#include "Room.hpp"
+
+class Player{
+    public:
+    Player() {};
+    ~Player() {};
+    Room* currentRoom;
+    vector<class Item*> inventory;
+}
+
+void setRoom(Room* curr) { currentRoom = curr; };
+void setItem(Item* item) { inventory.push_back(item); };
+
+
+
+#endif
